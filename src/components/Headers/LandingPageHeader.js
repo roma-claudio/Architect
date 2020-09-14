a65321cc0459b2b63079b926a7043516c9570e33
claudio.roma <claudio.roma@sophus3.com>
2020-09-14 21:53:10 +0100

    New - Adding public and src folders

---
+++ src/components/Headers/LandingPageHeader.js
@@ -0,0 +1,76 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import { Button, Container } from "reactstrap";
+
+// core components
+
+function LandingPageHeader() {
+  let pageHeader = React.createRef();
+
+  React.useEffect(() => {
+    if (window.innerWidth < 991) {
+      const updateScroll = () => {
+        let windowScrollTop = window.pageYOffset / 3;
+        pageHeader.current.style.transform =
+          "translate3d(0," + windowScrollTop + "px,0)";
+      };
+      window.addEventListener("scroll", updateScroll);
+      return function cleanup() {
+        window.removeEventListener("scroll", updateScroll);
+      };
+    }
+  });
+
+  return (
+    <>
+      <div
+        style={{
+          backgroundImage:
+              "url(" + require("assets/img/skyline.jpg") + ")",
+        }}
+        className="page-header"
+        data-parallax={true}
+        ref={pageHeader}
+      >
+        <div className="filter" />
+        <Container>
+          <div className="motto text-center">
+            <h1>Brand Name</h1>
+            <h3>Insert brand mission in this field.</h3>
+            <br />
+            <Button className="btn-round mr-5" color="neutral" outline>
+              Project 1
+            </Button>
+            <Button className="btn-round mr-5" color="neutral" type="button" outline>
+              Project 2
+            </Button>
+            <Button className="btn-round" color="neutral" type="button" outline>
+              Project 3
+            </Button>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default LandingPageHeader;
