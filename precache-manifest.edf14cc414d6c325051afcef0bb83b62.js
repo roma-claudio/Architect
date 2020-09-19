0dd520d6029b060c5ebf9d4b1b51c9140d97e8cf
roma-claudio <roma.claudio.it@gmail.com>
2020-09-19 23:01:49 +0100

    Updates

--- precache-manifest.edf14cc414d6c325051afcef0bb83b62.js
+++ precache-manifest.edf14cc414d6c325051afcef0bb83b62.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "81d495f5aa59b2aafbfc516b3843e114",
+    "revision": "612c9124d2bb3efc097cdb41b3256f86",
     "url": "/test/index.html"
   },
   {
-    "revision": "231e66145904ba7c3a01",
-    "url": "/test/static/css/main.d179caf7.chunk.css"
+    "revision": "d1c36bb9577bc9ee306d",
+    "url": "/test/static/css/main.91bdb7fd.chunk.css"
   },
   {
-    "revision": "64325185ece6d70f35d4",
-    "url": "/test/static/js/2.323aacfd.chunk.js"
+    "revision": "ae467c147ee3492d01dc",
+    "url": "/test/static/js/2.0b5efaab.chunk.js"
   },
   {
-    "revision": "161c6b8d7fe9f3d9d1c896db3e01ea03",
-    "url": "/test/static/js/2.323aacfd.chunk.js.LICENSE.txt"
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test/static/js/2.0b5efaab.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "231e66145904ba7c3a01",
-    "url": "/test/static/js/main.42408b5b.chunk.js"
+    "revision": "d1c36bb9577bc9ee306d",
+    "url": "/test/static/js/main.31acf5aa.chunk.js"
   },
   {
     "revision": "30f942bb7783c4b076a0017ec0876fab",
-    "url": "/test/static/js/main.42408b5b.chunk.js.LICENSE.txt"
+    "url": "/test/static/js/main.31acf5aa.chunk.js.LICENSE.txt"
   },
   {
     "revision": "aaa56efb021a30f2bbd1",
@@ -40,14 +40,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/test/static/media/clem-onojeghuo-3.ae11d928.jpg"
   },
   {
-    "revision": "61c4e0007254eb55e4691291881a4bfb",
-    "url": "/test/static/media/clouds.61c4e000.png"
-  },
-  {
-    "revision": "df0cdbfb2c70d9857e3776eb83a5a4eb",
-    "url": "/test/static/media/creative-tim-white-slim2.df0cdbfb.png"
-  },
-  {
     "revision": "2051e0e847124c536df9c01a3f672032",
     "url": "/test/static/media/erik-lucatero-2.2051e0e8.jpg"
   },
@@ -56,28 +48,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/test/static/media/fabio-mangione.f26c34bb.jpg"
   },
   {
-    "revision": "1331ba97560cc13103d379a8843ad1dc",
-    "url": "/test/static/media/federico-beccari.1331ba97.jpg"
-  },
-  {
-    "revision": "efe053682ec97d330adad93c55dc7bd0",
-    "url": "/test/static/media/ilya-yakover.efe05368.jpg"
-  },
-  {
     "revision": "76ff47ae5c64ccda5af18326bb272058",
     "url": "/test/static/media/joe-gardner-2.76ff47ae.jpg"
   },
   {
     "revision": "8a26aabcc3fe4dd8289d9f379c9f48c6",
     "url": "/test/static/media/joshua-stannard.8a26aabc.jpg"
-  },
-  {
-    "revision": "e06d84cb846db2a463ac3d904ac0c362",
-    "url": "/test/static/media/kaci-baum-2.e06d84cb.jpg"
-  },
-  {
-    "revision": "d65b63163c9caec8e994304bf1b1cfcc",
-    "url": "/test/static/media/landing-page.d65b6316.png"
   },
   {
     "revision": "be9fa468a029a195601370492c15519d",
@@ -98,10 +74,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
     "url": "/test/static/media/nucleo-icons.f32ed5ad.eot"
-  },
-  {
-    "revision": "ed8128994a7eed31e8ebeba831ed040c",
-    "url": "/test/static/media/profile-page.ed812899.png"
   },
   {
     "revision": "1525b6ef7ae79ef3791f3e7ebe0eeaf3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test/precache-manifest.f039846eb65d45c6ff4495470976f6bb.js"
+  "/test/precache-manifest.edf14cc414d6c325051afcef0bb83b62.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
